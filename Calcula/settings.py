adda633e1a94bd6e95458e23398676e4c1abbe9a
Alfredo Martinez <amartinez@isnstudio.com>
2021-04-29 20:29:19 -0600

    [UPDATE] admin static files

--- Calcula/settings.py
+++ Calcula/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,9 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 FIXTURE_DIRS = [BASE_DIR / 'fixtures']
 
